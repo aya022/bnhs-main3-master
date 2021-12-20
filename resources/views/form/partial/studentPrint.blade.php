4827593854673ac2befe5037c6d0d66564e65a17
Ailyn Manzano <manzanooailynn@gmail.com>
2021-12-20 22:31:45 +0800

    make it better

--- resources/views/form/partial/studentPrint.blade.php
+++ resources/views/form/partial/studentPrint.blade.php
@@ -23,7 +23,7 @@
                 <img src="{{ asset('image/logo/bn.jpg') }}" class="img-fluid rounded-circle" width="110%">
             </div>
             <div class="col-6 my-2 text-center">
-                <p class="mb-0 lead" style="font-size: 30px;">Balaogan National High School</p>
+                <b><p class="mb-0 lead" style="font-size: 30px;">Balaogan National High School</p></b>
                 <small class="lead" style="font-size: 20px;">Balaogan, Bula, Camarines Sur</small><br>
                 <small class="lead" style="font-size: 20px;">Region V</small><br><br>
                 <p style="font-size: 25px;"><b>Enrollment Form</b></p><br>
@@ -50,47 +50,48 @@
             <tbody>
                 @foreach ($dataNow as $key =>$student)
                 <tr>
-                    <td colspan="4"><b>Enrollment Information</b></td>
+                    <td colspan="4" style="padding: 10px;"><b>Enrollment Information</b></td>
                 </tr>
                 <tr>
-                    <td colspan="2">Enrollment No.</td>
-                    <td colspan="2">{{ $tracking_no }}</td>
+                    <td colspan="2" style="padding: 10px;">Enrollment No.</td>
+                    <td colspan="2" style="padding: 10px;">{{ $tracking_no }}</td>
                 </tr>
                 <tr>
-                    <td>Grade Level to Enroll:</td>
-                    <td>{{ $student->grade_level }}</td>
-                    <td>Status:</td>
-                    <td>{{ $student->state }}</td>
+                    <td style="padding: 10px;">Grade Level to Enroll:</td>
+                    <td style="padding: 10px;">{{ $student->grade_level }}</td>
+                    <td style="padding: 10px;">Status:</td>
+                    <td style="padding: 10px;">{{ $student->state }}</td>
                 </tr>
                 <tr>
-                    <td colspan="4"><b>Student Information</b></td>
+                    <td colspan="4" style="padding: 10px;"><b>Student Information</b></td>
                 </tr>
                 <tr>
-                    <td>Name:</td>
-                    <td>{{ $student->student_firstname }} {{ $student->student_middlename }} {{ $student->student_lastname }}</td>
-                    <td>Date of Birth</td>
-                    <td>{{ $student->date_of_birth }}</td>
+                    <td style="padding: 10px;">Name:</td>
+                    <td style="padding: 10px;">{{ $student->student_firstname }} {{ $student->student_middlename }} {{ $student->student_lastname }}</td>
+                    <td style="padding: 10px;">Date of Birth</td>
+                    <td style="padding: 10px;">{{ $student->date_of_birth }}</td>
                 </tr>
                 <tr>
-                    <td>Gender:</td>
-                    <td>{{ $student->gender }}</td> 
-                    <td>Learning Reference Number:</td>
-                    <td>{{ $student->roll_no }}</td>
+                    <td style="padding: 10px;">Gender:</td>
+                    <td style="padding: 10px;">{{ $student->gender }}</td> 
+                    <td style="padding: 10px;">Learning Reference Number:</td>
+                    <td style="padding: 10px;">{{ $student->roll_no }}</td>
                 </tr>
                 <tr>
-                    <td>Address:</td>
+                    <td style="padding: 10px;">Address:</td>
                     {{-- <td>{{ $student->region }}</td> --}}
-                    <td colspan="3">
+                    <td colspan="3" style="padding: 10px;">
+                        {{ $student->region }}, 
                         {{ $student->barangay }}, 
                         {{ $student->city }}, 
                         {{ $student->province }}
                     </td>
                 </tr>
                 <tr>
-                    <td>Last School Attended</td>
-                    <td>{{ $student->last_school_attended }}</td>
-                    <td>Balik Aral?</td>
-                    <td>{{ $student->isbalik_aral }}</td>
+                    <td style="padding: 10px;">Last School Attended</td>
+                    <td style="padding: 10px;">{{ $student->last_school_attended }}</td>
+                    <td style="padding: 10px;">Balik Aral?</td>
+                    <td style="padding: 10px;">{{ $student->isbalik_aral }}</td>
                 </tr>
                 <tr>
                     <td colspan="4"><b>Family Information</b></td>
@@ -106,6 +107,12 @@
                     <td>{{ $student->mother_name }}</td>
                     <td>Contact No.</td>
                     <td>{{ $student->mother_contact_no }}</td>
+                </tr>
+                <tr>
+                    <td>Guardian name:</td>
+                    <td>{{ $student->guardian_name }}</td>
+                    <td>Contact No.</td>
+                    <td>{{ $student->guardian_contact_no }}</td>
                 </tr>
                 @endforeach
             </tbody>
